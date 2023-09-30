533aa94061dd08019a954c567d8c59b7b6de90bb
massivegamechangers <134106451+massivegamechangers@users.noreply.github.com>
2023-09-30 14:20:04 +0200

    Service on updated

--- agrieasy/src/pages/Services.js
+++ agrieasy/src/pages/Services.js
@@ -12,7 +12,9 @@ const Services = () => {
         <img src="https://img.icons8.com/?size=100&id=wsaXI0aea252&format=gif" alt="" loading="" className="shadow-sm shadow-[#779867] rounded-full p-2" />
         <h3 className="ml-5">Service One </h3>
       </div>
-      <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facere quia pariatur, repudiandae sed cumque eos quaerat ipsum necessitatibus assumenda porro quas labore beatae velit accusamus accusantium, dolor dignissimos sequi?</p>
+      <p className="">As farmers requires an ongoing education to stay aware of the fast-moving developmemnt in technology,science, business management, and an array of other
+      skills and fields that affect agricultural operations.Funding opportunities to be implememnted through different incubation hubs,to give certifications to
+      existing farmers and new upcoming farmers. </p>
     </div>
 
     <div className="py-3 rounded-xl px-4 border-[#779867]">
