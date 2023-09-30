69d68e3d0f51e3f6202a44a9f8a66d2fcc9547b0
massivegamechangers <134106451+massivegamechangers@users.noreply.github.com>
2023-09-30 22:30:57 +0200

    FAQ page update in progress

--- agrieasy/src/pages/Faqs.js
+++ agrieasy/src/pages/Faqs.js
@@ -13,15 +13,17 @@ const Faqs = () => {
                 </div>
                 <div>
                     <h3 className="font-semibold text-[#779867]">How do I become a partner?</h3>
-                    <p className="mt-1 dark:text-[#000]">Do I need to have a farm to become a partner?, Are farmers clubs that on can engage with,to learn more about farmers lifestyle/</p>
+                    <p className="mt-1 dark:text-[#000]">Do I need to have a farm to become a partner?, Are farmers clubs that on can engage with,to learn more about farmers lifestyle?</p>
                 </div>
                 <div>
-                    <h3 className="font-semibold text-[#779867]">Lorem ipsum dolor sit amet.</h3>
-                    <p className="mt-1 dark:text-[#000]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+                    <h3 className="font-semibold text-[#779867]">What is farming and what are the different types of farming types?</h3>
+                    <p className="mt-1 dark:text-[#000]">What are the key duties of a farmer?,How do farmers know that the soil is fertile enough to be used to plant vegetables?,
+                    Is there a method or a measure of the fertility of the soil?</p>
                 </div>
                 <div>
-                    <h3 className="font-semibold text-[#779867]">Lorem ipsum dolor sit amet.</h3>
-                    <p className="mt-1 dark:text-[#000]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+                    <h3 className="font-semibold text-[#779867]">What are the problems faced by farmers in Soth africa?</h3>
+                    <p className="mt-1 dark:text-[#000]">Does our communities support our local farmers? Is there enough women in farming?, Is our government supporting our farmers?, Are there
+                    enough resources allocated to support farmers in our country?</p>
                 </div>
             </div>
         </div>
