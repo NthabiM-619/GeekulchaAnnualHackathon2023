b50564d21e95bc862c9aabb8b1144efb54aa3c2f
massivegamechangers <134106451+massivegamechangers@users.noreply.github.com>
2023-10-01 01:16:01 +0200

    Slogan updated

--- agrieasy/src/components/Hero.js
+++ agrieasy/src/components/Hero.js
@@ -1,93 +1,20 @@
-export default function( )  {
-  return(
-    <div className= "py-2 px mx-auto max-w-screen-md">
-    <h2 className="mb-4 text-4x1 font-extrabold
-    text-center text-gray-900">
-    Contact Us
-</h2>
-<p className="mb-4 font-light text text-left
-text-gray-500 sm: text-xl">
-Cultivating Ideas For Growth!
-</p>
-<form action="#">
-<div className="w-1/2 pr-2">
-<label for="firstName"
-className="block my-2 text-left
-text-sm font-medium text-gray-900">
+import React from 'react'
 
-First  Name
-</label>
-<input type="text"
-className="shadow-sm bg-gray-50 border
-border-gray-300 text-gray-900
-text-sm rounded-lg block w- full p-2.5"
-placeholder="Enter First Name"
-required/>
-</div>
-<div className="w-1/2 pl-2">
-<label for="firstName"
-className="block my-2 text-left text-sm
-font-medium text-gray-900">
-Last  Name
-</label>
-<input type="text"
-className="shadow-sm bg-gray-50 border
-border-gray-300 text-gray-900
-text-sm rounded-lg block w-full p-2.5"
-placeholder="Enter Last Name"/>
-</div>
-</div>
-</div>
-<label for="email"
-className="block my-2 text-left text-gray-900">
+const Hero = () => {
+  return (
+    <header className="flex justify-center" id='Home'>
+        <div className='text-center mt-36'>
+        <h1 className='text-9xl text-[#fbfaf9]'>AgriEase</h1>
+        <blockquote className='text-md'>Cultivating Ideas For Growth!</blockquote>
+        <p className='text-2xl mt-5'>Lorem jhbd w fj vfaebfhbemfbsejrf. erfjberjhgbs ejrfbjrhb g hserb gnerbhgbaef</p>
 
-Your Email
-</label>
-<input type="email"
-className="shadow-sm bg-gray-50 border
-border-gray-300 text-gray-900
-text-sm rounded-lg block w-full p-2.5"
-placeholder="abcd@gmail.com"
-required />
-</div>
-<div/>
-<label for="subject
-className="block my-2 text-left
-text-sm font-medium text-gray-900">
-
-Subject
-</label>
-<input type="text"
-className="block p-3 w-full text-sm
-text-gray-900 bg-gray-50 rounded-lg
-border border-gray-300 shadow-sm "
-placeholder="What issues or suggestions  do you have?"
-required />
-</div>
-<div>
-</div>
-<label for="message"
-className="block my-2 text-left  text-sm font-medium text-gray-900 ">
-
-Your Message
-
-</label>
-
-<textarea rows="6"
-className="block p-2.5 w-full text-sm
-text-gray-900 bg-gray-50 rounded-lg
-shadow-sm border border-gray-300 "
-placeholder="Questions/Suggestions..."/>
-
-</div>
-<button type="submit"
-className="mt-2 p-2 float-right text-white
-rounded-lg border-green-600
-bg-green-600 hover:scale-105">
-Send message
-</button>
-
-
-
+        <div className='flex w-full justify-center [&>*]:mx-16 [&>*]:w-32 text-white mt-20 [&>*]:p-4'>
+          <button className='border'>Agri Training</button>
+          <button className='border'>Seller</button>
+        </div>
+        </div>
+    </header>
   )
-})
+}
+
+export default Hero 
