144dfe25142bf2781027e0f2ff8d94e6ea440f79
massivegamechangers <134106451+massivegamechangers@users.noreply.github.com>
2023-10-01 01:20:37 +0200

    slogan updated

--- agrieasy/src/components/Hero.js
+++ agrieasy/src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className='text-center mt-36'>
         <h1 className='text-9xl text-[#fbfaf9]'>AgriEase</h1>
         <blockquote className='text-md'>Cultivating Ideas For Growth!</blockquote>
-        <p className='text-2xl mt-5'>Lorem jhbd w fj vfaebfhbemfbsejrf. erfjberjhgbs ejrfbjrhb g hserb gnerbhgbaef</p>
+        <p className='text-2xl mt-5'>Creating a transformative mobile App for Farmer</p>
 
         <div className='flex w-full justify-center [&>*]:mx-16 [&>*]:w-32 text-white mt-20 [&>*]:p-4'>
           <button className='border'>Agri Training</button>
