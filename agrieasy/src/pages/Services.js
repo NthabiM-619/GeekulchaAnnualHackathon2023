8ea37d1163b0ff268fd79ac62f264fbe337b701b
massivegamechangers <134106451+massivegamechangers@users.noreply.github.com>
2023-09-30 21:57:03 +0200

    services update in progress

--- agrieasy/src/pages/Services.js
+++ agrieasy/src/pages/Services.js
@@ -22,11 +22,8 @@ const Services = () => {
         <img src="https://img.icons8.com/?size=100&id=L7fD8ebkUJt0&format=gif" alt="" loading="" className="shadow-sm shadow-[#779867] rounded-full p-2" />
         <h3 className="ml-5">Service Three</h3>
       </div>
-      <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facere quia pariatur, repudiandae sed cumque eos quaerat ipsum necessitatibus assumenda porro quas labore beatae velit accusamus accusantium, dolor dignissimos sequi?</p>
-    </div>
-
-    <div className="py-3 rounded-xl px-4 border-[#779867]">
-      <div className="flex items-center mb-2">
+      <p className="">Users makes payments into our account, then a fee is charged for transfer from our account to farmers virtual accounts.</p>
+          <div className="flex items-center mb-2">
         <img src="https://img.icons8.com/?size=100&id=Wyq9ckwhhDZB&format=gif" alt="" loading="" className="shadow-sm shadow-[#779867] rounded-full p-2" />
         <h3 className="ml-5">Service Two</h3>
       </div>
