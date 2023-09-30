dc6e0e261952c4dfaf70c7a07a62ff489364f716
TreciaKS <tksekgalo@gmail.com>
2023-09-30 11:07:09 +0000

    faqs designed

--- agrieasy/src/pages/Home.js
+++ agrieasy/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import About from './About'
 import Services from './Services'
+import Faqs from './Faqs'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <About />
       <Services />
+      <Faqs />
     </section>
   )
 }
